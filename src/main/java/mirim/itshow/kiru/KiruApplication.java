cc8342cb2bfbb4fa3bc28f47ec72c08237d9f462
지민 백 <s2104@e-mirim.hs.kr>
2023-04-19 17:37:41 +0900

    Initial commit

---
+++ src/main/java/mirim/itshow/kiru/KiruApplication.java
@@ -0,0 +1,13 @@
+package mirim.itshow.kiru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mirim/itshow/kiru/KiruApplicationTests.java
@@ -0,0 +1,13 @@
+package mirim.itshow.kiru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

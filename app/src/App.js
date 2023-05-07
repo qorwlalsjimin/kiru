27e58427c55cf55232f452442bc80b7c28d977e2
seoyoung <wkdtjdud312@gmail.com>
2023-05-08 08:37:49 +0900

    app.js merge 문제 해결

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,67 @@
-import './App.css';
-import {useEffect, useState} from "react";
-import axios from "axios";
+import React from 'react';
+import "./App.css"
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Pages from './components/pages/Pages'
+import Header from '/components/common/header/Header'
+import Tradition from './components/goods_list/tradition/Tradition'
+//import { formToJSON } from 'axios'
+//import axios from 'axios'
 
-const App = () => {
+
+function App()  {
   return (
+    <>
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path='/groups' exact={true} element={<GroupList/>}/>
-        <Route path='/groups/:id' element={<GroupEdit/>}/>
-      </Routes>
-    </Router>
+      <Header />
+      <Switch>
+      <Route path='/tradition' exact component = {Tradition} />
+
+      </Switch>
+
+      </Router>
+    </>
   )
 }
 
-export default App;
+export default App
+// const App = () => {
+//   const [message, setMessage] = useState([]);
+
+//   useEffect(()=>{
+//     fetch("/pages")
+//     .then((response =>{
+//         return response.json();
+//     })
+//     .then(function (data){
+//         setMessage(data);
+//     }));
+// },[]);
+
+
+
+//   return (
+//     <>
+//       <Pages />
+
+//     </>
+//   )
+// }
+
+
+// function App() {
+//   const [hello, setHello] = useState('')
+
+//    useEffect(() => {
+//        axios.get('/api/hello')
+//        .then(response => setHello(response.data))
+//        .catch(error => console.log(error))
+//    }, []);
+
+//    return (
+//        <div>
+//            백엔드에서 가져온 데이터입니다 : {hello}
+//        </div>
+//    );
+// }
+

8fd62886616bb9732b9674512dfd74a72168708f
지민 백 <s2104@e-mirim.hs.kr>
2023-06-13 00:06:10 +0900

    즐겨찾기 페이지 html+css 틀 잡기

--- src/main/java/mirim/itshow/kiru/app/src/pages/Heart.jsx
+++ src/main/java/mirim/itshow/kiru/app/src/pages/Heart.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
-export default function Heart(){
+export default function Heart() {
 
-    return(
+    // 상품 목록 조회
+    let [country, setCountry] = useState('hanbok');
+    let fetchUrl = `/api/heart/all/${country}`;
+
+    // 상품 데이터
+    let [products, setProducts] = useState({});
+
+    // 카테고리 목록
+    let [categorys, setCategorys] = useState({});
+    
+    // 상품 데이터 가져오기
+    useEffect(() => {
+        const fetchData = async (url) => {
+            try {
+                const response = await axios.get(url);
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData(fetchUrl);
+    }, [country]);
+
+
+    function countryHandler(param) {
+        console.log("백: ", param);
+        setCountry(param); //hanbok, kimono로 바꿈
+        categoryDatas(); //카테고리 목록 가져오기
+    }
+
+      // 나라별 카테고리 목록 가져오기
+    async function categoryDatas() {
+    try {
+      const response = await axios.get(`/api/heart/all/${country}`);
+      console.log(response);
+      setCategorys(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+    return (
         <>
-        즐찾!
+            <div className='Banner'>
+                <h2>즐겨찾기</h2>
+
+                {/* 한복 기모노 정렬 */}
+                <div className="selection">
+                    <div className="cloum c1" onClick={countryHandler.bind(this, 'hanbok')}>
+                        <i className="ri-checkbox-blank-circle-fill"></i>
+                        한복
+                    </div>
+                    <div className="cloum c2" onClick={countryHandler.bind(this, 'kimono')}>
+                        <i className="ri-checkbox-blank-circle-fill"></i>
+                        기모노
+                    </div>
+                </div>
+            </div>
+
+
+            {/* 카테고리 목록 */}
+            {/* <nav className="brand_nav">
+                {Object.values(categorys).map((category) => {
+                    return (
+                        <span key={category.categoryId} onClick={brandHandler.bind(this, brand.categoryId)} style={{ marginRight: "10px" }}>{brand.title}</span>
+                    );
+                })}
+            </nav> */}
         </>
     )
 }

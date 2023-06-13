763e896d90c94b8306e23a69cb5d0e6d98c05994
지민 백 <s2104@e-mirim.hs.kr>
2023-06-13 11:03:17 +0900

    검색 모달

--- src/main/java/mirim/itshow/kiru/app/src/components/header/topNavigationBar/topNavigationBar.jsx
+++ src/main/java/mirim/itshow/kiru/app/src/components/header/topNavigationBar/topNavigationBar.jsx
@@ -1,29 +1,47 @@
-// import styles from "./topNavigationBar.module.css";
-// import { Link, Navigate } from "react-router-dom";
-import {nav} from "../../../data/Data"
 import "./header.css"
 import "./script"
 import { Link, Navigate, Routes, Router, Route } from "react-router-dom";
-import axios from 'axios';
-// import Main from "../main/main";
-import {Main} from "../../main/main"
-// import { Redirect} from "react-router-dom";
-import { Product } from "../../products/product";
+import { Main } from "../../main/main"
 import { Mainscreen } from "../../mainscreen/Mainscreen";
 
+
 //svg
-import {ReactComponent as SearchSvg} from "../../../svgfiles/search.svg";
-import {ReactComponent as StarSvg} from "../../../svgfiles/star.svg";
-import {ReactComponent as CartSvg} from "../../../svgfiles/cart.svg";
-import {ReactComponent as MemberSvg} from "../../../svgfiles/member.svg";
-import { useState } from "react";
+import { ReactComponent as SearchSvg } from "../../../svgfiles/search.svg";
+import { ReactComponent as StarSvg } from "../../../svgfiles/star.svg";
+import { ReactComponent as CartSvg } from "../../../svgfiles/cart.svg";
+import { ReactComponent as MemberSvg } from "../../../svgfiles/member.svg";
+import { useEffect, useRef, useState } from "react";
+import SearchModal from "./SearchModal";
 
 
-const Header = ({ handleNavClick, setShowMainscreen, setProducts, products}) => {
+const Header = ({ handleNavClick, setShowMainscreen, setProducts, products }) => {
   let [isBrand, setIsBrand] = useState(true);
   function navHandler() {
     setIsBrand(true);
   }
+
+  // 검색 Modal
+  const [show, setShow] = useState(0);
+
+  const open = (e) => {
+    setShow(1);
+  };
+
+  const ref = useRef();
+
+  useEffect(() => {
+    const modlalClose = (e) => {
+      if (show && ref.current && !ref.current.contains(e.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", modlalClose);
+    return () => {
+      document.removeEventListener("mousedown", modlalClose);
+    };
+  }, [show]);
+
+
   return (
     <>
       <header>
@@ -32,7 +50,7 @@ const Header = ({ handleNavClick, setShowMainscreen, setProducts, products}) => 
           <div className='global'>
             <i className="ri-global-line"></i> 한국어
           </div>
-         
+
           <div className="logo">
             <Link to="/Mainscreen">
               <img src="/images/logo.png" alt="" />
@@ -41,26 +59,62 @@ const Header = ({ handleNavClick, setShowMainscreen, setProducts, products}) => 
 
           {/* 우측 아이콘들 */}
           <div className="toggle">
-            <i><SearchSvg width={"20px"}/></i> {/* 검색 */}
-            <i><Link to="/heart"><StarSvg/></Link></i> {/* 즐겨찾기 */}
-            <i><Link to="/cart"><CartSvg width={"19px"}/></Link></i> {/* 장바구니 */}
-            <i><Link to="/login_form"><MemberSvg width={"20px"}/></Link></i> {/* 로그인/회원가입 */}
+            <i><SearchSvg width={"20px"} onClick={open} /></i> {/* 검색 */}
+            {/* 검색 Modal */}
+            {show ? (
+              <div className="search_modal" show={show} ref={ref}>
+                <div className="modla-desk">
+                  <div className="search_area">
+                    <form>
+                      <input></input>
+                      <button type="submit">🔍</button>
+                    </form>
+                  </div>
+                  {/* --search_area */}
+                  <div className="result_area">
+                    <ul>
+                      <li>
+                        <span>🔍</span>
+                        <span>검색어</span>
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;X</span>
+                      </li>
+                      <li>
+                        <span>🔍</span>
+                        <span>검색어</span>
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;X</span>
+                      </li>
+                      <li>
+                        <span>🔍</span>
+                        <span>검색어</span>
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;X</span>
+                      </li>
+                    </ul>
+                  </div>{" "}
+                  {/* --result_area */}
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+            <i><Link to="/heart"><StarSvg /></Link></i> {/* 즐겨찾기 */}
+            <i><Link to="/cart"><CartSvg width={"19px"} /></Link></i> {/* 장바구니 */}
+            <i><Link to="/login_form"><MemberSvg width={"20px"} /></Link></i> {/* 로그인/회원가입 */}
           </div>
         </div>
 
         <div className="nav">
-        <ul className="flex">
-          <li onClick={navHandler}><Link to="/category/120/all"><button>개량</button></Link></li>
-          <li onClick={navHandler}><Link to="/category/110/all"><button >전통</button></Link></li>
-          <li onClick={navHandler}><Link to="/category/130/all"><button >신발</button></Link></li>
-          <li onClick={navHandler}><Link to="/category/140/all"><button >악세사리</button></Link></li>
-        </ul>
+          <ul className="flex">
+            <li onClick={navHandler}><Link to="/category/120/all"><button>개량</button></Link></li>
+            <li onClick={navHandler}><Link to="/category/110/all"><button >전통</button></Link></li>
+            <li onClick={navHandler}><Link to="/category/130/all"><button >신발</button></Link></li>
+            <li onClick={navHandler}><Link to="/category/140/all"><button >악세사리</button></Link></li>
+          </ul>
         </div>
         <Routes>
-        <Route path="/" element={<Mainscreen />} />
+          <Route path="/" element={<Mainscreen />} />
           <Route path="/category/:cid/:cname" element={<Main products={products} setProducts={setProducts} isBrand={isBrand} setIsBrand={setIsBrand} />} />
-      </Routes>
-    
+        </Routes>
+
       </header>
     </>
   )

f3746107625c569b8fdcb8d175b15fc6141ddcc4
지민 백 <s2104@e-mirim.hs.kr>
2023-06-13 19:07:12 +0900

    파일 정리

--- src/main/java/mirim/itshow/kiru/app/src/App.jsx
+++ src/main/java/mirim/itshow/kiru/app/src/App.jsx
@@ -7,46 +7,12 @@ import Product from "./pages/product";
 import Basket from "./pages/basket";
 import Login_form from "./components/member/Login_form";
 import Join_form from "./components/member/Join_form";
-import Modernized from "./components/item/modernized";
-// import Tradition from "./components/item/Tradition";
-import Shoes from './components/item/shoes'
-import Accessories from "./components/item/Accessories"
-import { Main } from "./components/main/main";
-import axios from 'axios';
 import { Mainscreen } from "./components/mainscreen/Mainscreen"
 import Heart from "./pages/Heart";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  // const [showMainscreen, setShowMainscreen] = useState(true);
-  // const [isMainscreenVisible, setMainscreenVisible] = useState(true);
-
-  // const handleNavClick = async (nav) => {
-  //   let url = '';
-
-  //   if (nav === '전통한복') {
-  //     url = '/api/item/item_list/110';
-  //   } else if (nav === '개량한복') {
-  //     url = '/api/item/item_list/120';
-  //   } else if (nav === '신발') {
-  //     url = '/api/item/item_list/130';
-  //   } else if (nav === '악세사리') {
-  //     url = '/api/item/item_list/140';
-  //   } else if (nav === '세트') {
-  //     url = '/api/item/item_list/150';
-  //   }
-
-  //   try {
-  //     const response = await axios.get(url);
-  //     setProducts(response.data);
-  //   } catch (error) {
-  //     console.error('Error fetching data:', error);
-  //   }
-
-  //   setShowMainscreen(false);
-
-  // };
 
   const handleNavClick = async (nav, fetchData) => {
     fetchData(nav);

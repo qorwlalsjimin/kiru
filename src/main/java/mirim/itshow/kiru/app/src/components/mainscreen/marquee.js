3657480379e610e167ce2c4e8095be6a92996ee3
wkdtjdujd <wkdtjdud312@gmail.com>
2023-06-21 16:16:23 +0900

    카테고리 캐러셀 완성

--- src/main/java/mirim/itshow/kiru/app/src/components/mainscreen/marquee.js
+++ src/main/java/mirim/itshow/kiru/app/src/components/mainscreen/marquee.js
@@ -2,49 +2,4 @@ import $ from "jquery";
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
 
-// const carousel = document.querySelector(".carousel");
 
-// const leftArrow = document.querySelector(".left-arrow");
-// const rightArrow = document.querySelector(".right-arrow");
-
-// let currentIndex = 0;
-// let prevIndex;
-// const images = document.querySelectorAll(".carousel-image");
-
-// const totalImages = Object.keys(images).length;
-
-
-// const imageWidth = 520;
-
-// leftArrow.addEventListener("click", () => {
-//   prevIndex = currentIndex;
-//   currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-//   carousel.style.transform = `translateX(-${imageWidth}px)`;
-//   carousel.insertBefore(images[currentIndex], carousel.firstChild);
-
-//   setTimeout(() => {
-//     carousel.style.transform = "";
-//     carousel.classList.add("sliding-transition");
-    
-//   }, 10);
-
-//   setTimeout(() => {
-//     carousel.classList.remove("sliding-transition");
-//   }, 490);
-// });
-
-// rightArrow.addEventListener("click", () => {
-//   carousel.classList.add("sliding-transition");
-
-//   prevIndex = currentIndex;
-//   currentIndex = (currentIndex + 1) % totalImages;
-
-//   carousel.style.transform = `translateX(-${imageWidth}px)`;
- 
-
-//   setTimeout(() => {
-//     carousel.appendChild(images[prevIndex]);
-//     carousel.classList.remove("sliding-transition");
-//     carousel.style.transform = "";
-//   }, 500);
-// });

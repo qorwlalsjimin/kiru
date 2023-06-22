3d5faca14d165e22e5cfe214849606a3940fb722
지민 백 <s2104@e-mirim.hs.kr>
2023-06-23 07:52:30 +0900

    전시를 위해 expire time 10시간으로 수정

--- src/main/java/mirim/itshow/kiru/jwt/TokenProvider.java
+++ src/main/java/mirim/itshow/kiru/jwt/TokenProvider.java
@@ -27,7 +27,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30 * 20;            // 30분 * 20 = 600분 10시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
     private final Key key;
